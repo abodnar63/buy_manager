23e1481cbebd5cdd7d89b2347c225bc188020b9a
Anton Bodnar <abodnar@inflection.com>
2017-07-26 23:45:04 +0300

    fixed labels url

--- app/assets/javascripts/pages/dashboard/models/label.js
+++ app/assets/javascripts/pages/dashboard/models/label.js
@@ -1,3 +1,9 @@
 Dashboard.models.Label = Backbone.Model.extend({
-  urlRoot: "/labels"
+  urlRoot: "/labels",
+  
+  initialize: function() {
+    if (this.collection) {
+      this.urlRoot = null;
+    }
+  }
 })
--- app/models/labels_product.rb
+++ app/models/labels_product.rb
@@ -1,6 +1,6 @@
 class LabelsProduct < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :label
+  belongs_to :product, dependent: :destroy
+  belongs_to :label, dependent: :destroy
 
   validates :product_id, presence: true
   validates :label_id, presence: true
